ed770baf2ff0cb2b035e2deaade00be400911728
Adrielly Julião <adriellyvespa15@gmail.com>
2024-10-17 20:46:27 -0300

    starting home screen

--- src/SimplonBarberShop/app/index.tsx
+++ src/SimplonBarberShop/app/index.tsx
@@ -10,7 +10,7 @@ const App = () => {
       <ScrollView
         contentContainerStyle={stylesHeader.header}
       >
-        <Text style={{color: 'white'}}>Hello World ADRIELLY JULIÃO Pedro henrique Yuri Lindo</Text>
+        <Text style={{color: 'white'}}>Criando Tela inicial</Text>
       </ScrollView>
       <StatusBar  style="light" />
     </SafeAreaView>
