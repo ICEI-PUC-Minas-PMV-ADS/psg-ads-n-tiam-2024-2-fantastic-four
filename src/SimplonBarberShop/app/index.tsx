16c3df1ef3243292fa4bb1f7c6af2ac6a34b90a0
Adrielly Julião <adriellyvespa15@gmail.com>
2024-10-19 11:10:24 -0300

    layout initial screen

--- src/SimplonBarberShop/app/index.tsx
+++ src/SimplonBarberShop/app/index.tsx
@@ -1,32 +1,100 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
-import { Colors } from "@/constants/Colors";
-import CustomInput from "@/components/customInput";
 import { router } from "expo-router";
+import customButton from "../components/customButton";
 
-const App = () => {
+const App = (navigation: any) => {
   return (
-    <SafeAreaView style={{height: '100%'}}>
-      <ScrollView
-        contentContainerStyle={stylesHeader.header}
-      >
-        <Text style={{color: 'white'}}>Criando Tela inicial</Text>
+    <SafeAreaView style={{ height: "100%", backgroundColor: "#121212" }}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View
+          style={{
+            width: "100%",
+            alignItems: "center",
+            minHeight: "100%",
+          }}
+        >
+          <Image
+            source={require("../assets/images/barberPole.png")}
+            style={styles.background}
+            resizeMode="cover"
+          />
+
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={styles.logo}
+          />
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => router.push("/(auth)/sign-in")}
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.button, styles.registerButton]}
+            onPress={() => router.push("/(auth)/sign-up")}
+          >
+            <Text style={styles.registerButtonText}>Registrar</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
-      <StatusBar style="light" />
     </SafeAreaView>
   );
 };
 
-export default App;
-
-export const stylesHeader = StyleSheet.create({
-  header: {
+const styles = StyleSheet.create({
+  background: {
+    width: 450,
+    minHeight: 650,
+  },
+  container: {
+    height: "100%",
+    backgroundColor: "#121212",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    height: "100%",
-    backgroundColor: Colors.backgroundScreen,
+  },
+  logo: {
+    width: 200,
+    height: 212,
+    position: "absolute",
+    top: "55%",
+  },
+  button: {
+    width: 310,
+    padding: 15,
+    backgroundColor: "#AE8333",
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 20,
+    top: 50,
+  },
+  registerButton: {
+    backgroundColor: "transparent",
+    borderColor: "#AE8333",
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 20,
+    fontFamily: "CircularSpotifyText-Bold"
+  },
+  registerButtonText: {
+    color: "#AE8333",
+    fontSize: 20,
+    fontFamily: "CircularSpotifyText-Bold"
   },
 });
+
+export default App;
