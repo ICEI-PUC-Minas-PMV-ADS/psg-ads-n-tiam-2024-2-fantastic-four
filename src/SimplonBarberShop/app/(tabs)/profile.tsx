4feefdd2d2cea1072dfb731957f2a413badd17fc
Yuri Lucas Silva Ferreira <yuri.ferreira@telefonica.com>
2024-10-30 20:14:29 -0300

    profile

--- src/SimplonBarberShop/app/(tabs)/profile.tsx
+++ src/SimplonBarberShop/app/(tabs)/profile.tsx
@@ -1,15 +1,135 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import MobileLayout from '@/components/layout/mobileLayout'
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { MaterialIcons, Feather } from "@expo/vector-icons";
+import MobileLayout from "@/components/layout/mobileLayout";
+import { useAuthContext } from "../context/authContextProvider";
+import firebase from "../../service/firebaseConnection";
+import CustomButton from "@/components/customButton";
+import { router } from "expo-router";
 
-const Profile = () => {
-  return (
-    <MobileLayout>
-      <Text>Profile</Text>
-    </MobileLayout>
-  )
+// Definindo a interface para os dados do usuário
+interface UserData {
+  nome: string;
+  dataNascimento: string;
+  telefone: string;
 }
 
-export default Profile
+const Profile = () => {
+  const { user } = useAuthContext();
 
-const styles = StyleSheet.create({})
+  return (
+    <MobileLayout>
+      <View style={styles.container}>
+        {user && (
+          <>
+            <View style={styles.profileHeader}>
+              <View style={styles.avatar}>
+                <MaterialIcons name="account-circle" size={80} color="white" />
+              </View>
+              <View>
+                <Text style={styles.name}>{user.nome}</Text>
+              </View>
+            </View>
+            <View style={styles.infoContainer}>
+              <View style={styles.infoRow}>
+                <Feather name="mail" size={20} color="white" />
+                <Text style={styles.infoText}>{user.email}</Text>
+              </View>
+              <View style={styles.infoRow}>
+                <Feather name="phone" size={20} color="white" />
+                <Text style={styles.infoText}>{user.telefone}</Text>
+              </View>
+              <View style={styles.infoRow}>
+                <Feather name="calendar" size={20} color="white" />
+                <Text style={styles.infoText}>{user.dataNascimento}</Text>
+              </View>
+            </View>
+            <View>
+              <CustomButton
+                title={"Editar Perfil"}
+                onPress={() => router.push("../historic")}
+                width={164}
+                backgroundColor={"#121212"}
+                border={1}
+                borderColor="#D2B070"
+                iconSize={19}
+                textColor="#D2B070"
+              />
+            </View>
+            <View style={styles.footer}>
+              <Text style={styles.footerText}>Redefinir Senha</Text>
+              <Text style={styles.footerText}>Sair do Aplicativo</Text>
+            </View>
+          </>
+        )}
+      </View>
+    </MobileLayout>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  profileHeader: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  name: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  age: {
+    color: "#A9A9A9",
+    fontSize: 16,
+  },
+  infoContainer: {
+    marginVertical: 20,
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  infoText: {
+    color: "#fff",
+    fontSize: 16,
+    marginLeft: 10,
+  },
+  editButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#A89D5F",
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  editButtonText: {
+    color: "#fff",
+    marginLeft: 5,
+  },
+  footer: {
+    borderTopWidth: 1,
+    borderTopColor: "#333",
+    paddingVertical: 10,
+    alignItems: "center",
+  },
+  footerText: {
+    color: "#A9A9A9",
+    fontSize: 16,
+    marginVertical: 5,
+  },
+});
